4e4cd7a5f3c66fc0cdc27edb9dfbf21eb9f06c58
a97490rp <radu.pirlog@student.manchester.ac.uk>
2020-10-28 20:05:27 +0000

    Main menu is done

---
+++ coursework_01/Spellcheck.py
@@ -0,0 +1,19 @@
+import os.path
+print("Press 1 if you want to spell check a sentence.", "\nPress 2 if you want to spell check a file.", "\nPress 0 if you want to quit the program.")
+var = int(input())
+if(var > 2 or var < 0):
+	print("Invalid entry. Try again!")
+	var = input()
+while(var != 0):
+	if(var == 1):
+		sentence = input("PLease input the sentence: ")
+		print(sentence)
+		break
+	elif(var == 2):
+		filePath = input("Please enter the file name: ")
+		if(os.path.isfile(filePath)):
+			sentence = open(filePath, 'r')
+			print(sentence.read())
+			sentence.close()
+			break
+print("Program terminated!")
