9092f07aa01175369cd1d6f3dddf03c1ffc47799
a97490rp <radu.pirlog@student.manchester.ac.uk>
2020-10-28 20:13:30 +0000

    m=Menu perfected

--- coursework_01/Spellcheck.py
+++ coursework_01/Spellcheck.py
@@ -1,9 +1,9 @@
 import os.path
 print("Press 1 if you want to spell check a sentence.", "\nPress 2 if you want to spell check a file.", "\nPress 0 if you want to quit the program.")
 var = int(input())
-if(var > 2 or var < 0):
+while(var > 2 or var < 0):
 	print("Invalid entry. Try again!")
-	var = input()
+	var = int(input())
 while(var != 0):
 	if(var == 1):
 		sentence = input("PLease input the sentence: ")
@@ -16,4 +16,6 @@ while(var != 0):
 			print(sentence.read())
 			sentence.close()
 			break
+		else:
+			print("Invalid path. Try again!")
 print("Program terminated!")
